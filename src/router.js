26eb062491c4eae27c4864946d5588da141b1b21
Raph Ramirez <ralph@s2dioapps.com>
2020-12-15 23:28:55 +0800

    refactoring project structure 1

--- src/router.js
+++ src/router.js
@@ -47,96 +47,96 @@ export default new Router({
         {
           name: 'RTL',
           path: 'pages/rtl',
-          component: () => import('@/views/dashboard/pages/Rtl'),
+          component: () => import('@/views/dashboard/components/pages/Rtl'),
         },
         {
           name: 'User Profile',
           path: 'pages/user',
-          component: () => import('@/views/dashboard/pages/UserProfile'),
+          component: () => import('@/views/dashboard/components/pages/UserProfile'),
         },
         {
           name: 'Timeline',
           path: 'pages/timeline',
-          component: () => import('@/views/dashboard/pages/Timeline'),
+          component: () => import('@/views/dashboard/components/pages/Timeline'),
         },
         // Components
         {
           name: 'Buttons',
           path: 'components/buttons',
-          component: () => import('@/views/dashboard/component/Buttons'),
+          component: () => import('@/views/dashboard/components/component/Buttons'),
         },
         {
           name: 'Grid System',
           path: 'components/grid-system',
-          component: () => import('@/views/dashboard/component/Grid'),
+          component: () => import('@/views/dashboard/components/component/Grid'),
         },
         {
           name: 'Tabs',
           path: 'components/tabs',
-          component: () => import('@/views/dashboard/component/Tabs'),
+          component: () => import('@/views/dashboard/components/component/Tabs'),
         },
         {
           name: 'Notifications',
           path: 'components/notifications',
-          component: () => import('@/views/dashboard/component/Notifications'),
+          component: () => import('@/views/dashboard/components/component/Notifications'),
         },
         {
           name: 'Icons',
           path: 'components/icons',
-          component: () => import('@/views/dashboard/component/Icons'),
+          component: () => import('@/views/dashboard/components/component/Icons'),
         },
         {
           name: 'Typography',
           path: 'components/typography',
-          component: () => import('@/views/dashboard/component/Typography'),
+          component: () => import('@/views/dashboard/components/component/Typography'),
         },
         // Forms
         {
           name: 'Regular Forms',
           path: 'forms/regular',
-          component: () => import('@/views/dashboard/forms/RegularForms'),
+          component: () => import('@/views/dashboard/components/forms/RegularForms'),
         },
         {
           name: 'Extended Forms',
           path: 'forms/extended',
-          component: () => import('@/views/dashboard/forms/ExtendedForms'),
+          component: () => import('@/views/dashboard/components/forms/ExtendedForms'),
         },
         {
           name: 'Validation Forms',
           path: 'forms/validation',
-          component: () => import('@/views/dashboard/forms/ValidationForms'),
+          component: () => import('@/views/dashboard/components/forms/ValidationForms'),
         },
         {
           name: 'Wizard',
           path: 'forms/wizard',
-          component: () => import('@/views/dashboard/forms/Wizard'),
+          component: () => import('@/views/dashboard/components/forms/Wizard'),
         },
         // Tables
         {
           name: 'Regular Tables',
           path: 'tables/regular-tables',
-          component: () => import('@/views/dashboard/tables/RegularTables'),
+          component: () => import('@/views/dashboard/components/tables/RegularTables'),
         },
         {
           name: 'Extended Tables',
           path: 'tables/extended-tables',
-          component: () => import('@/views/dashboard/tables/ExtendedTables'),
+          component: () => import('@/views/dashboard/components/tables/ExtendedTables'),
         },
         {
           name: 'Data Tabels',
           path: 'tables/data-tables',
-          component: () => import('@/views/dashboard/tables/DataTables'),
+          component: () => import('@/views/dashboard/components/tables/DataTables'),
         },
         // Maps
         {
           name: 'Google Maps',
           path: 'maps/google-maps',
-          component: () => import('@/views/dashboard/maps/GoogleMaps'),
+          component: () => import('@/views/dashboard/components/maps/GoogleMaps'),
         },
         {
           name: 'Full Screen Map',
           path: 'maps/full-screen-map',
-          component: () => import('@/views/dashboard/maps/FullScreenMap'),
+          component: () => import('@/views/dashboard/components/maps/FullScreenMap'),
         },
         // Root level
         {
