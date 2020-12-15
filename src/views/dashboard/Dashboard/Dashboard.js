b5596d2d43cad57f4629a35536f1d81fb42f7e51
Raph Ramirez <ralph@s2dioapps.com>
2020-12-15 23:35:20 +0800

    refactoring project structure 2

---
+++ src/views/dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'Dashboard',
+
+  data () {
+    return {}
+  },
+}
---
+++ src/views/dashboard/Dashboard/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container
+    id="dashboard"
+    fluid
+    tag="section"
+  >
+    <v-row>
+      <h1>Hello, World!</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+</script>
