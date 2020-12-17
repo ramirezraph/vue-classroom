e60589e6f945e1946b944ab17a56977396a29514
rsl081 <53944109+rsl081@users.noreply.github.com>
2020-12-17 13:08:11 +0800

    add announcement ui

--- src/components/base/MaterialCard.vue
+++ src/components/base/MaterialCard.vue
@@ -38,7 +38,7 @@
 
         <div
           v-else-if="title && !icon"
-          class="display-1 font-weight-light"
+          class="text-center display-1 font-weight-light"
           v-text="title"
         />
 
