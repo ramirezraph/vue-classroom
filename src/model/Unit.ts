8718e62599858c0ef10313d706f0220190d39473
Raph Ramirez <ralph@s2dioapps.com>
2021-01-19 18:54:58 +0800

    added id for unit & lesson

--- src/model/Unit.ts
+++ src/model/Unit.ts
@@ -5,6 +5,7 @@ export class Unit {
 
   // eslint-disable-next-line no-useless-constructor
   constructor (
+    public id: string,
     public unitNumber: number,
     public title: string,
     public shortDescription: string,
