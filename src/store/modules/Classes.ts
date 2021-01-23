c26dd99c3b4a1f241d97409d8e87515883f1450c
Raphael Ramirez <ramirez.raph@yahoo.com>
2021-01-24 00:19:23 +0800

    set class data realtime

--- src/store/modules/Classes.ts
+++ src/store/modules/Classes.ts
@@ -21,28 +21,26 @@ export default {
   },
   actions: {
      fetchClasses (context: { commit: (arg0: string, arg1: Class[]) => void }, payload: User) {
-      const classes: Class[] = []
-      classesCollection.where('userList', 'array-contains', payload.id).get().then(function (querySnapshot) {
-          querySnapshot.forEach(function (doc) {
-            if (doc.exists) {
-              const generatedClass = new Class(
-                doc.id,
-                doc.data().title,
-                doc.data().description,
-                doc.data().code,
-                doc.data().teacherName,
-                doc.data().imageSource,
-                doc.data().color
-              )
-              classes.push(generatedClass)
-            }
-          })
-        })
-        .catch(function (error) {
-          console.log('Error getting documents: ', error)
-        })
+       classesCollection.where('userList', 'array-contains', payload.id)
+         .onSnapshot(snapshot => {
+           const classes: Class[] = []
+           snapshot.forEach(doc => {
+             if (doc.exists) {
+               const generatedClass = new Class(
+                 doc.id,
+                 doc.data().title,
+                 doc.data().description,
+                 doc.data().code,
+                 doc.data().teacherName,
+                 doc.data().imageSource,
+                 doc.data().color
+               )
+               classes.push(generatedClass)
+             }
+           })
 
-      context.commit('SET_CLASSES', classes)
+           context.commit('SET_CLASSES', classes)
+         })
     },
   },
 }
