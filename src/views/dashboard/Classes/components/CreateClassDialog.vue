ce615153dba4742bb3ecc9a9306632d5b194bd56
Raphael Ramirez <ramirez.raph@yahoo.com>
2021-01-25 20:27:49 +0800

    added create class ui
    > Select image still not working.

--- src/views/dashboard/Classes/components/CreateClassDialog.vue
+++ src/views/dashboard/Classes/components/CreateClassDialog.vue
@@ -26,11 +26,12 @@
             cols="6"
           >
             <class-header
-              color="blue"
-              title="Software Design"
-              description="Lorem ipsum dolor sit amet."
-              code="CPE 302"
-              teacher="Raphael Ramirez"
+              :color="displayColor"
+              :title="displayTitle"
+              :description="displayDescription"
+              :code="displayCode"
+              :teacher="teacherName"
+              :image="displayImage"
             />
           </v-col>
         </v-row>
@@ -40,43 +41,117 @@
             cols="6"
           >
             <v-card>
-              <v-card-title class="text-left">
-                Please fill up the form completely.
+              <v-card-title class="text-left grey--text">
+                <span class="caption">Please fill out the required fields.</span>
               </v-card-title>
-            <v-card-text class="pa-6">
-                <validation-provider
+              <v-card-text class="pa-6">
+                <validation-observer
                   ref="observer"
                   v-slot="{ invalid }"
                 >
-                  <validation-provider
-                    v-slot="{ errors }"
-                    name="Class Code"
-                    rules="required"
-                  >
-                    <v-text-field
-                      width="150"
-                      single-line
-                      :error-messages="errors"
-                      label="Class Code"
-                      color="blue"
-                      outlined
-                      style="width: 600px"
-                    />
-                  </validation-provider>
-                  <v-btn
-                    min-width="200px"
-                    color="blue"
-                    :disabled="invalid"
-                    type="submit"
-                  >
-                    Create Class
-                  </v-btn>
-                  <v-btn
-                    text
-                  >
-                    Cancel
-                  </v-btn>
-                </validation-provider>
+                  <v-form @submit.prevent="">
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Code"
+                      rules="required"
+                    >
+                      <v-text-field
+                        v-model="code"
+                        :error-messages="errors"
+                        label="Class Code"
+                        color="blue"
+                        outlined
+                      />
+                    </validation-provider>
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Title"
+                      rules="required"
+                    >
+                      <v-text-field
+                        v-model="title"
+                        :error-messages="errors"
+                        label="Class Title"
+                        color="blue"
+                        outlined
+                      />
+                    </validation-provider>
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Description"
+                    >
+                      <v-text-field
+                        v-model="description"
+                        :error-messages="errors"
+                        label="Class Description"
+                        color="blue"
+                        outlined
+                        hint="Optional"
+                      />
+                    </validation-provider>
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Teacher"
+                      rules="required"
+                    >
+                      <v-text-field
+                        v-model="teacherName"
+                        :error-messages="errors"
+                        label="Class Teacher"
+                        color="blue"
+                        outlined
+                        disabled
+                      />
+                    </validation-provider>
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Background Image"
+                    >
+                      <v-file-input
+                        v-model="imageSource"
+                        :error-messages="errors"
+                        accept="image/*"
+                        label="Class Background Image"
+                        type="file"
+                        color="blue"
+                        hint="Optional"
+                        prepend-icon=""
+                        prepend-inner-icon="mdi-image"
+                        persistent-hint
+                        outlined
+                      />
+                    </validation-provider>
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Class Color"
+                      rules="required"
+                    >
+                      <v-text-field
+                        v-model="color"
+                        :error-messages="errors"
+                        label="Class Color"
+                        color="blue"
+                        outlined
+                      />
+                    </validation-provider>
+                    <div class="mt-6">
+                      <v-btn
+                        min-width="200px"
+                        color="blue"
+                        :disabled="invalid"
+                        type="submit"
+                      >
+                        Create Class
+                      </v-btn>
+                      <v-btn
+                        text
+                        @click="cancel"
+                      >
+                        Cancel
+                      </v-btn>
+                    </div>
+                  </v-form>
+                </validation-observer>
               </v-card-text>
             </v-card>
           </v-col>
@@ -88,8 +163,15 @@
 
 <script lang="ts">
   import Vue from 'vue'
-  import { ValidationObserver, ValidationProvider } from 'vee-validate'
+  import { extend, ValidationObserver, ValidationProvider } from 'vee-validate'
   import ClassHeader from '@/views/dashboard/Class/components/ClassHeader.vue'
+  import { required } from 'vee-validate/dist/rules'
+
+  extend('required', {
+    ...required,
+    message: '{_field_} is required.',
+  })
+
   export default Vue.extend({
     name: 'CreateClassDialog',
     components: {
@@ -105,12 +187,32 @@
     },
     data () {
       return {
-
+        title: '',
+        description: '',
+        code: '',
+        teacherName: 'Raphael Ramirez',
+        imageSource: null,
+        color: '',
       }
     },
     computed: {
-      modelData () {
+      modelData (): boolean {
         return this.vModel
+      },
+      displayTitle (): string {
+        return this.title || 'Class Title'
+      },
+      displayDescription (): string {
+        return this.description || 'Class Description'
+      },
+      displayCode (): string {
+        return this.code || 'Class Code'
+      },
+      displayColor (): string {
+        return this.color || 'blue'
+      },
+      displayImage (): string {
+        return ''
       },
     },
     methods: {
