0d6255816f6f7e62bccc1a3a1e9e84acfdd4b9f3
Alternis-Dimen <hanzlacruz@gmail.com>
2021-02-04 23:24:54 +0800

    added class list ui

--- src/views/dashboard/Dashboard/components/DashboardClassList.vue
+++ src/views/dashboard/Dashboard/components/DashboardClassList.vue
@@ -1,18 +1,26 @@
 <template>
   <v-card>
     <v-card-title>
-      <div class="d-flex flex-wrap align-center">
-        <v-icon
-          color="green lighten-2"
-          x-large
-        >
-          ballot
-        </v-icon>
-        <span class="display-2 ml-4">Classes</span>
+      <div class="d-flex">
+        <div>
+          <v-icon
+            color="green lighten-2"
+            class="ml-8 mt-5"
+            x-large
+          >
+            ballot
+          </v-icon>
+        </div>
+        <div>
+          <h1 class="ml-6 mt-7">
+            Classes
+          </h1>
+        </div>
       </div>
     </v-card-title>
     <div class="d-flex align-end flex-column">
       <v-text-field
+        v-model="search"
         placeholder="Search"
         append-icon="mdi-magnify"
         class="shrink mr-6"
@@ -25,7 +33,34 @@
           hide-default-footer
           :headers="headers"
           :items="classes"
-        />
+          :search.sync="search"
+          :sort-by="['name']"
+          multi-sort
+        >
+          <template #[`item.teacher`]="{ item }">
+            <v-chip
+              dark
+              class="short blue white--text justify-center"
+              style="width: 60%;"
+            >
+              <span>{{ item.teacher }}</span>
+            </v-chip>
+          </template>
+          <template #[`item.actions`]="{ item }">
+            <v-icon
+              small
+              class="mr-2"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
       </v-tabs-items>
     </v-card-text>
   </v-card>
@@ -41,63 +76,53 @@
           { text: '#', value: 'counter' },
           { text: 'Subject Name', value: 'name' },
           { text: 'Subject Code', value: 'code' },
-          { text: 'Credit/Hours', value: 'credits' },
+          { text: 'Credits/Hours', value: 'credits' },
           { text: 'Teacher', value: 'teacher' },
-          { text: 'Actions', value: 'actions' },
+          { text: 'Actions', value: 'actions', sortable: false },
         ],
         classes: [
           {
             counter: '1',
             name: 'Software Design',
-            code: 'CPE303',
+            code: 'CPE301',
             credits: 4,
             teacher: 'John L. Doe',
             actions: '',
           },
           {
             counter: '1',
-            name: 'Software Design',
-            code: 'CPE303',
+            name: 'Data Structure and Algorithms',
+            code: 'CPE302',
             credits: 4,
-            teacher: 'John L. Doe',
+            teacher: 'John Titor',
             actions: '',
           },
           {
             counter: '1',
-            name: 'Software Design',
-            code: 'CPE303',
+            name: 'Differential Calculus',
+            code: 'CPE304',
             credits: 4,
-            teacher: 'John L. Doe',
+            teacher: 'Lee Harvey Oswald',
             actions: '',
           },
           {
             counter: '1',
-            name: 'Software Design',
-            code: 'CPE303',
+            name: 'Operating System',
+            code: 'CPE305',
             credits: 4,
-            teacher: 'John L. Doe',
+            teacher: 'Michael Macintosh',
             actions: '',
           },
           {
             counter: '1',
-            name: 'Software Design',
-            code: 'CPE303',
+            name: 'Engineering Economy',
+            code: 'CPE306',
             credits: 4,
-            teacher: 'John L. Doe',
+            teacher: 'Rudolph Fentz',
             actions: '',
           },
         ],
-        editItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialog = true
-        },
-
-        deleteItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialogDelete = true
-        },
+        search: undefined,
       }
     },
   })
