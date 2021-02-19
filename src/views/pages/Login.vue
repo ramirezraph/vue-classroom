8f32054abdc1553577fdce8255bd62d831555376
Raph Ramirez <ralph@s2dioapps.com>
2021-02-19 15:57:15 +0800

    login form ui complete

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,10 +1,12 @@
 <template>
   <v-card
-    class="ma-0 pa-0 px-12"
+    class="ma-0 pa-0"
     flat
   >
-    <v-row>
-      <v-col cols="3">
+    <v-row class="justify-center">
+      <v-col
+        cols="3"
+      >
         <v-img
           class="fill-height pa-10"
           src="../../assets/login-bg-3.jpg"
@@ -12,7 +14,7 @@
         >
           <div
             id="appMotto"
-            class="text-h2 white--text font-weight-medium"
+            class="text-h2 white--text"
           >
             <div class="d-block">
               Never
@@ -40,7 +42,7 @@
           </div>
         </v-img>
       </v-col>
-      <v-col cols="6">
+      <v-col cols="5">
         <v-img
           class="pa-10"
           src="../../assets/login-bg-1.jpg"
@@ -49,13 +51,97 @@
           min-height="100%"
         >
           <v-card
-            class="fill-height mt-0"
-            rounded
+            id="login-card"
+            class="fill-height mt-0 px-12"
             flat
           >
-            <v-card-title>
-              <span class="text-h3 ma-auto">Login to your account</span>
+            <v-card-title class="py-12">
+              <span class="text-h3 ma-auto mt-12 black--text">Login to your account</span>
             </v-card-title>
+            <v-card-text>
+              <v-form @submit.prevent="onLogin">
+                <v-text-field
+                  v-model="email"
+                  prepend-icon="mdi-email-outline"
+                  label="Email"
+                />
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  prepend-icon="mdi-lock-outline"
+                  type="password"
+                />
+                <div
+                  class="d-flex"
+                >
+                  <div>
+                    <v-checkbox
+                      v-model="rememberMe"
+                      label="Remember Me"
+                    />
+                  </div>
+                  <div class="ml-auto my-auto">
+                    <v-btn
+                      text
+                      small
+                      class="subtitle-1 grey--text text-none"
+                    >
+                      <v-icon left>
+                        mdi-key
+                      </v-icon>
+                      Forgot Password
+                    </v-btn>
+                  </div>
+                </div>
+                <v-btn
+                  depressed
+                  color="primary"
+                  block
+                  class="subtitle-1 text-none mt-6"
+                  min-height="50"
+                  type="submit"
+                >
+                  Login
+                </v-btn>
+              </v-form>
+              <span class="text-h5 d-block text-center mt-8 grey--text">Or login with</span>
+              <div class="d-flex text-center justify-center mt-6">
+                <div>
+                  <v-btn
+                    icon
+                    x-large
+                    color="primary"
+                  >
+                    <v-icon x-large>
+                      mdi-google
+                    </v-icon>
+                  </v-btn>
+                </div>
+                <div>
+                  <v-btn
+                    icon
+                    x-large
+                    color="primary"
+                  >
+                    <v-icon x-large>
+                      mdi-facebook
+                    </v-icon>
+                  </v-btn>
+                </div>
+              </div>
+              <div
+                class="d-block text-center mt-16"
+              >
+                <v-btn
+                  text
+                  depressed
+                  width="200"
+                  class="text-h4 primary--text text-none"
+                >
+                  Sign Up
+                </v-btn>
+              </div>
+            </v-card-text>
           </v-card>
         </v-img>
       </v-col>
@@ -63,18 +149,29 @@
   </v-card>
 </template>
 
-<script label="ts">
+<script lang="ts">
   import Vue from 'vue'
   export default Vue.extend({
     data () {
       return {
-
+        email: '',
+        password: '',
+        rememberMe: false,
       }
+    },
+    methods: {
+      onLogin (): void {
+        console.log('login submit')
+      },
     },
   })
 </script>
 
 <style lang="scss" scoped>
+  #login-card {
+    border-radius: 3%;
+  }
+
   #appMotto {
     position: absolute;
     bottom: 3%;
